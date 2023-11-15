b4fc3d07f5b66cd75e5aa3d49e98fc8773b1b279
Nick Pavlou <99483163+nickpavlou@users.noreply.github.com>
2023-11-15 14:13:34 +0000

    Search and Pagination Functionality Added

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { ShopComponent } from './shop.component';
@@ -7,7 +8,7 @@ import { ProductItemComponent } from './product-item/product-item.component';
 
 @NgModule({
     declarations: [ShopComponent, ProductItemComponent],
-    imports: [CommonModule, FontAwesomeModule],
+    imports: [CommonModule, SharedModule, FontAwesomeModule],
     exports: [ShopComponent],
 })
 export class ShopModule {}
