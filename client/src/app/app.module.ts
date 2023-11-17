8f895cfd4b672a29f0f7ebc4a46138fdc608dbd7
Nick Pavlou <99483163+nickpavlou@users.noreply.github.com>
2023-11-17 23:34:02 +0000

    Error Handling

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,15 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
 
 import { HomeModule } from './home/home.module';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ErrorInterceptor } from './core/interceptors/error.interceptor';
 
 @NgModule({
     declarations: [AppComponent],
     imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, HttpClientModule, CoreModule, HomeModule],
-    providers: [],
+    providers: [{ provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -3,10 +3,23 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { RouterModule } from '@angular/router';
+import { TestErrorComponent } from './test-error/test-error.component';
+import { ContactComponent } from './contact/contact.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ServerErrorComponent } from './server-error/server-error.component';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
-    declarations: [NavbarComponent],
-    imports: [CommonModule, FontAwesomeModule, RouterModule],
+    declarations: [NavbarComponent, TestErrorComponent, ContactComponent, NotFoundComponent, ServerErrorComponent],
+    imports: [
+        CommonModule,
+        FontAwesomeModule,
+        RouterModule,
+        ToastrModule.forRoot({
+            positionClass: 'toast-bottom-right',
+            preventDuplicates: true,
+        }),
+    ],
     exports: [NavbarComponent],
 })
 export class CoreModule {}
